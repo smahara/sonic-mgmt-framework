46ca1d65345bf467da6a5eff42ed0c02c94e7b57
Rohit Jayadevan <rohit.jayadevan@broadcom.com>
2019-05-22 18:12:44 -0700

    Revert "Changes to SONiC to be able to fetch SAI debian packages from a local path"
    This reverts commit 264c100565c31abb809284eba11ef0add9fe80a4.
    
    Reason for revert: Didn't work well with Santosh's 3.6 SAI headers
    
    Change-Id: Ic932772d01da8cf59cdb3ad3fc4bbe2d64ddaba2

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,27 +1,9 @@
-BRCM_SAI =  libsaibcm_3.3.5.4m-1_amd64.deb
-BRCM_SAI_DEV =  libsaibcm-dev_3.3.5.4m-1_amd64.deb
-SAI_LIBS_MOUNT_POINT = /sai_libs
+BRCM_SAI = libsaibcm_3.6.0.1_amd64.deb
+$(BRCM_SAI)_URL = "http://10.59.132.240:9999/projects/csg_sonic/sonicbld/ocp_sai_190522_0300/build/libsaibcm_3.6.0.1_amd64.deb"
 
-ifneq  ($(LOCAL_SAI_DEBS_PATH),)
-override BRCM_SAI = $(shell find $(SAI_LIBS_MOUNT_POINT) -maxdepth 1 -regex '.*libsaibcm_[0-9.]+_amd64\.deb' -printf "%f")
-override BRCM_SAI_DEV = $(shell find $(SAI_LIBS_MOUNT_POINT) -maxdepth 1 -regex '.*libsaibcm-dev_[0-9.]+_amd64\.deb' -printf "%f")
-endif
-
-#BRCM_SAI
-ifeq ($(LOCAL_SAI_DEBS_PATH),)
-$(BRCM_SAI)_URL = "http://10.59.132.240:9009/projects/csg_sonic/sonic_builds/sai_debs/ocp_sai_190522_0300/build/libsaibcm-dev_3.6.0.1_amd64.deb"
-SONIC_ONLINE_DEBS += $(BRCM_SAI)
-else
-$(BRCM_SAI)_PATH = $(SAI_LIBS_MOUNT_POINT)
-SONIC_COPY_DEBS += $(BRCM_SAI)
-endif
-
-#BRCM_SAI_DEV
+BRCM_SAI_DEV = libsaibcm-dev_3.6.0.1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-ifeq ($(LOCAL_SAI_DEBS_PATH),)
-$(BRCM_SAI_DEV)_URL = "http://10.59.132.240:9009/projects/csg_sonic/sonic_builds/sai_debs/ocp_sai_190522_0300/build/libsaibcm-dev_3.6.0.1_amd64.deb"
-else
-$(BRCM_SAI_DEV)_PATH = $(SAI_LIBS_MOUNT_POINT)
-endif
+$(BRCM_SAI_DEV)_URL = "http://10.59.132.240:9999/projects/csg_sonic/sonicbld/ocp_sai_190522_0300/build/libsaibcm-dev_3.6.0.1_amd64.deb"
 
+SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
