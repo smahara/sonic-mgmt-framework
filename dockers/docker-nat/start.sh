854acf10c7e915fe3c545a5574797d902ee6fabb
Akhilesh Samineni <akhilesh.samineni@broadcom.com>
2019-06-25 01:39:06 -0700

    Enabled the natmgrd and natsyncd inside NAT docker.
    Change-Id: Idf8e1fc4528da684a376090b4b62ca1823ae52b8

--- dockers/docker-nat/start.sh
+++ dockers/docker-nat/start.sh
@@ -3,7 +3,11 @@
 rm -f /var/run/rsyslogd.pid
 rm -f /var/run/nat/*
 
+mkdir -p /var/warmboot/nat
+
 supervisorctl start rsyslogd
 
-#supervisorctl start natmgrd
+supervisorctl start natmgrd
+
+supervisorctl start natsyncd
 
