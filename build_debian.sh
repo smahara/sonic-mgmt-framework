72f94daabed381975d807194549052e29d888937
Kalimuthu Velappan <kalimuthu.velappan@broadcom.com>
2019-06-28 02:55:08 -0700

    Support for tech-support export to an external server
    Change-Id: I405a5b848e8fd7f824a84219ed6d51da311c4aae

--- build_debian.sh
+++ build_debian.sh
@@ -438,6 +438,11 @@ sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip install 'docker
 ## Get gcc and python dev pkgs
 sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y install gcc libpython2.7-dev
 
+
+# Package scp/sftp/crypto for export
+sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT pip install paramiko SSHClient SCPClient scp watchdog pycrypto  Daemonize
+
+
 ## Create /var/run/redis folder for docker-database to mount
 sudo mkdir -p $FILESYSTEM_ROOT/var/run/redis
 
