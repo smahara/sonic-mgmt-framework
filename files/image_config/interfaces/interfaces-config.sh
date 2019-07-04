749b3ebe3198e38ec094629b721d8703c0a0ae1c
Rajendra Dendukuri <rajendra.dendukuri@broadcom.com>
2019-07-04 14:02:46 -0700

    [JIRA SONIC-5796] ZTP taking around 6 to 9 mins of time to conclude the result (SUCCESS / FAILURE) after reboot
    - Do not add interfaces that do not exist in /etc/network/interfaces
    - Do not start DHCP client on in-band interfaces which are not linked up
    - This speeds up interfaces-config service during bootup
    - Interfaces are later added by ZTP which waits for all the interfaces to be
    created and then issues 'systemctl restart interfaces-config' command
    - This saves approximately 35s of total time taken to perform a ZTP operation
    
    Signed-off-by: Rajendra Dendukuri <rajendra.dendukuri@broadcom.com>
    Change-Id: If5fc2ec1051235d7bbd103a65b9be832e2fcf100

--- files/image_config/interfaces/interfaces-config.sh
+++ files/image_config/interfaces/interfaces-config.sh
@@ -2,7 +2,22 @@
 
 ifdown --force eth0
 
-sonic-cfggen -d -t /usr/share/sonic/templates/interfaces.j2 > /etc/network/interfaces
+# Obtain port operational state information
+redis-dump -d 0 -k "PORT_TABLE:Ethernet*"  -y > /tmp/port_table_data.json
+if [ ! -e /tmp/port_table_data.json ] || \
+   [ "$(cat /tmp/port_table_data.json)" = "" ]; then
+   echo "{}" > /tmp/port_table_data.json
+fi
+
+# Create an input file with port operational state information
+echo "{ \"PORT_TABLE\" : $(cat /tmp/port_table_data.json) }" > \
+      /tmp/port_table.json
+
+# Create /e/n/i file for existing and active interfaces
+sonic-cfggen -d -j /tmp/port_table.json -t /usr/share/sonic/templates/interfaces.j2 > /etc/network/interfaces
+
+# Clean-up created files
+rm -f /tmp/port_table.json /tmp/port_table.json
 
 [ -f /var/run/dhclient.eth0.pid ] && kill `cat /var/run/dhclient.eth0.pid` && rm -f /var/run/dhclient.eth0.pid
 [ -f /var/run/dhclient6.eth0.pid ] && kill `cat /var/run/dhclient6.eth0.pid` && rm -f /var/run/dhclient6.eth0.pid
