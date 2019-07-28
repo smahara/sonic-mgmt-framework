b68f881512d3f731b38d2db68177b65b22d562c4
Rajendra Dendukuri <rajendra.dendukuri@broadcom.com>
2019-07-28 08:43:08 -0700

    [JIRA SONIC-6984] Clean-up ZTP configuration on service exit
    - ZTP config is stored temporarily in Config DB. When the service is exiting,
    remove the ZTP config from running config as well as start-up config
    - Also added additional checks in ztp profile creation for /e/n/i file,
    not to start DHCP on interfaces if ZTP DHCP profile is not active.
    
    Signed-off-by: Rajendra Dendukuri <rajendra.dendukuri@broadcom.com>
    Change-Id: I99eb815aa708ba8f2fc12ea1620987b72be5f9cd

--- files/image_config/interfaces/interfaces-config.sh
+++ files/image_config/interfaces/interfaces-config.sh
@@ -2,22 +2,24 @@
 
 ifdown --force eth0
 
-# Obtain port operational state information
-redis-dump -d 0 -k "PORT_TABLE:Ethernet*"  -y > /tmp/port_table_data.json
-if [ ! -e /tmp/port_table_data.json ] || \
-   [ "$(cat /tmp/port_table_data.json)" = "" ]; then
-   echo "{}" > /tmp/port_table_data.json
+# Check if ZTP DHCP policy has been installed
+if [ -e /etc/network/ifupdown2/policy.d/ztp_dhcp.json ]; then
+    # Obtain port operational state information
+    redis-dump -d 0 -k "PORT_TABLE:Ethernet*"  -y > /tmp/ztp_port_data.json
+
+    if [ ! -e /tmp/ztp_port_data.json ] || [ "$(cat /tmp/ztp_port_data.json)" = "" ]; then
+        echo "{}" > /tmp/ztp_port_data.json
+    fi
+
+    # Create an input file with ztp input information
+    echo "{ \"PORT_DATA\" : $(cat /tmp/ztp_port_data.json) }" > \
+          /tmp/ztp_input.json
+else
+    echo "{ \"ZTP_DHCP_DISABLED\" : \"true\" }" > /tmp/ztp_input.json
 fi
 
-# Create an input file with port operational state information
-echo "{ \"PORT_TABLE\" : $(cat /tmp/port_table_data.json) }" > \
-      /tmp/port_table.json
-
 # Create /e/n/i file for existing and active interfaces
-sonic-cfggen -d -j /tmp/port_table.json -t /usr/share/sonic/templates/interfaces.j2 > /etc/network/interfaces
-
-# Clean-up created files
-rm -f /tmp/port_table.json /tmp/port_table.json
+sonic-cfggen -d -j /tmp/ztp_input.json -t /usr/share/sonic/templates/interfaces.j2 > /etc/network/interfaces
 
 [ -f /var/run/dhclient.eth0.pid ] && kill `cat /var/run/dhclient.eth0.pid` && rm -f /var/run/dhclient.eth0.pid
 [ -f /var/run/dhclient6.eth0.pid ] && kill `cat /var/run/dhclient6.eth0.pid` && rm -f /var/run/dhclient6.eth0.pid
@@ -26,7 +28,10 @@ for intf_pid in $(ls -1 /var/run/dhclient*.Ethernet*.pid 2> /dev/null); do
   [ -f ${intf_pid} ] && kill `cat ${intf_pid}` && rm -f ${intf_pid}
 done
 
-sonic-cfggen -d -t /usr/share/sonic/templates/dhclient.conf.j2 > /etc/dhcp/dhclient.conf
+sonic-cfggen -d -j /tmp/ztp_input.json -t /usr/share/sonic/templates/dhclient.conf.j2 > /etc/dhcp/dhclient.conf
 systemctl restart networking
+
+# Clean-up created files
+rm -f /tmp/ztp_input.json /tmp/ztp_port_data.json
 
 ifdown lo && ifup lo
