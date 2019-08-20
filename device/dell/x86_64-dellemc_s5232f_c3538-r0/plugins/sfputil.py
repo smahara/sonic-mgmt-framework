a95792389ddc546f31401a08463aff3f9cdbd77a
Srideep Devireddy <srideep_devireddy@dell.com>
2019-08-20 14:21:43 -0700

    [JIRA SONIC-8947]  Bug fix for Z9264 crash and SFP+ presence detection                    Bug fix for S5232 crash and SFP+ presence detection
    Change-Id: Ib5735d24ccea5fdd5f14c372e70011772be02d51

--- device/dell/x86_64-dellemc_s5232f_c3538-r0/plugins/sfputil.py
+++ device/dell/x86_64-dellemc_s5232f_c3538-r0/plugins/sfputil.py
@@ -112,6 +112,11 @@ class SfpUtil(SfpUtilBase):
         # Mask off 4th bit for presence
         mask = (1 << 4)
 
+        # Mask off 1st bit for presence 33,34
+        if (port_num > 32):
+            mask =  (1 << 0)
+
+
         # ModPrsL is active low
         if reg_value & mask == 0:
             return True
