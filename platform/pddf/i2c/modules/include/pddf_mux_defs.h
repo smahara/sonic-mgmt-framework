5a1af49f288456fe41b6d89f0c92b2ee96dd567e
Fuzail Khan <fuzail.khan@broadcom.com>
2019-09-19 02:00:43 -0700

    [JIRA SONIC-9677] Adding support for IX1 and IX8 platform bringup using PDDF
    Change-Id: I262e5b6531a9734746394ef970c6e528742ff9ae

--- platform/pddf/i2c/modules/include/pddf_mux_defs.h
+++ platform/pddf/i2c/modules/include/pddf_mux_defs.h
@@ -3,16 +3,18 @@
  * The term “Broadcom” refers to Broadcom Inc. and/or its subsidiaries.
  *
  * Description:
- *	Platform MUX defines/structures header file
+ *  Platform MUX defines/structures header file
  */
 
 #ifndef __PAL_MUX_DEFS_H__
 #define __PAL_MUX_DEFS_H__
 
+#include <linux/i2c/pca954x.h>
+
 /* MUX CLIENT DATA - PLATFORM DATA FOR PSU CLIENT */
 typedef struct MUX_DATA
 {
-	int virt_bus;		// Virtual base bus number of the mux channels
+    int virt_bus;       // Virtual base bus number of the mux channels
 }MUX_DATA;
 
 #endif //__PAL_MUX_DEFS_H__
