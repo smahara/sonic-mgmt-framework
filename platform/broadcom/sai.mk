05527b9efbb17e0231082a7882e7ca0734cacf17
Satish Kumar Koti <satish-k.kumar@broadcom.com>
2019-10-18 11:12:58 -0700

    [JIRA SONIC-7137] online hwdiag support changes for asic component
    Sanity_Logs:
    http://10.59.132.240:9009/projects/csg_sonic/sanity/logs/dev-sanity-th2-td3-03/sonic_dev_sanity_191023_1806_1791
    
    Change-Id: I8aaea0d9c52a1e0bc114dddcd740ae02fb9fa161

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,10 +1,12 @@
 BRCM_SAI = libsaibcm_3.255.6.2_amd64.deb
 BRCM_SAI_DEV = libsaibcm-dev_3.255.6.2_amd64.deb
+BRCM_SAI_DIAG = libsaibcm-diag_3.255.6.2_amd64.deb
 SAI_LIBS_MOUNT_POINT = /sai_libs
 
 ifneq  ($(LOCAL_SAI_DEBS_PATH),)
 override BRCM_SAI = $(shell find $(SAI_LIBS_MOUNT_POINT) -maxdepth 1 -regex '.*libsaibcm_[0-9.]+_amd64\.deb' -printf "%f")
 override BRCM_SAI_DEV = $(shell find $(SAI_LIBS_MOUNT_POINT) -maxdepth 1 -regex '.*libsaibcm-dev_[0-9.]+_amd64\.deb' -printf "%f")
+override BRCM_SAI_DIAG = $(shell find $(SAI_LIBS_MOUNT_POINT) -maxdepth 1 -regex '.*libsaibcm-diag_[0-9.]+_amd64\.deb' -printf "%f")
 endif
 
 #BRCM_SAI
@@ -25,3 +27,12 @@ $(BRCM_SAI_DEV)_PATH = $(SAI_LIBS_MOUNT_POINT)
 endif
 
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
+
+#BRCM_SAI_DIAG
+ifeq ($(LOCAL_SAI_DEBS_PATH),)
+$(BRCM_SAI_DIAG)_URL = "http://10.59.132.240:9009/projects/csg_sonic/sonic_builds/sai_debs/LATEST_INT_OCP_SAI_3.X/build/$(BRCM_SAI_DIAG)"
+SONIC_ONLINE_DEBS += $(BRCM_SAI_DIAG)
+else
+$(BRCM_SAI_DIAG)_PATH = $(SAI_LIBS_MOUNT_POINT)
+SONIC_COPY_DEBS += $(BRCM_SAI_DIAG)
+endif
