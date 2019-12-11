1ce65573ead051295fb8338a762bc1184802436b
Martin Belanger <martin_belanger@dell.com>
2019-12-11 14:43:38 -0500

    add exception handling

--- src/ham/libham/ham_userdel.cpp
+++ src/ham/libham/ham_userdel.cpp
@@ -3,6 +3,8 @@
 #include "dbus-proxy.h"
 #include "../shared/dbus-address.h" /* DBUS_BUS_NAME_BASE, DBUS_OBJ_PATH_BASE */
 
+#include <syslog.h>
+
 int ham_userdel(const char * login)
 {
     DBus::BusDispatcher         dispatcher;
@@ -10,7 +12,18 @@ int ham_userdel(const char * login)
     DBus::Connection conn    = DBus::Connection::SystemBus();
 
     accounts_proxy_c interface(conn, DBUS_BUS_NAME_BASE, DBUS_OBJ_PATH_BASE);
-    ::DBus::Struct< bool, std::string > ret = interface.userdel(login);
+
+    ::DBus::Struct<bool, std::string> ret;
+
+    try
+    {
+        ret = interface.userdel(login);
+    }
+    catch (DBus::Error & ex)
+    {
+        syslog(LOG_CRIT, "ham_userdel(login=\"%s\" - Exception %s\n", login, ex.what());
+        ret._1 = false;
+    }
 
     return ret._1;
 }
