d9a56a849b787cf30fa428eadf2a05d2b4a3447b
Kalimuthu Velappan <kalimuthu.velappan@broadcom.com>
2020-01-05 07:58:35 -0800

    [JIRA SONIC-14946] Disabling the netlink filter in vSONIC mode
    Change-Id: Id76915aedd94dda6dac68e338a2ee33917e22547

--- src/libebpf/src/libutil/libebpf.c
+++ src/libebpf/src/libutil/libebpf.c
@@ -42,7 +42,8 @@ __attribute__ ((visibility("default"))) int attach_ebpf_filter(int nl_fd, char *
 
 	if (load_bpf_file(filename)) {
 		printf("%s", bpf_log_buf);
-		return -1;
+		printf("Filter attach is failed for %s socket \n", filename);
+		return 0;
 	}
 
 	assert(setsockopt(nl_fd, SOL_SOCKET, SO_ATTACH_BPF, prog_fd, sizeof(prog_fd[0])) == 0);
