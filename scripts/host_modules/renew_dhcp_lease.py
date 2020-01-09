1b3749826a74718865dedcef420f64f4910b0cd7
Ravi Vasanthm <ravi_vasanthm@dell.com>
2020-01-09 11:00:36 -0800

    Fixed issue in returning the status from host module script.

--- scripts/host_modules/renew_dhcp_lease.py
+++ scripts/host_modules/renew_dhcp_lease.py
@@ -28,19 +28,15 @@ class RENEW_DHCP_LEASE(host_service.HostModule):
                     cmd_opt = "-D LL"
 
                 cmd = "/sbin/dhclient {} -r {}".format(version, ifName)
-                print("RENEW_DHCP_LEASE - cmd {}".format(cmd))
                 output = subprocess.check_call(cmd, shell=True)
-                print('RENEW_DHCP_LEASE release Output -> ', output)
 
                 cmd = "[ -f /var/run/dhclient{}.{}.pid ] && kill `cat /var/run/dhclient{}.{}.pid` && rm -f /var/run/dhclient{}.{}.pid".format(file_ext, ifName, file_ext, ifName, file_ext, ifName)
-                print("RENEW_DHCP_LEASE - cmd {}".format(cmd))
                 output = subprocess.check_call(cmd, shell=True)
-                print('RENEW_DHCP_LEASE release Output -> ', output)
 
                 cmd = "/sbin/dhclient {} -pf /run/dhclient{}.{}.pid -lf /var/lib/dhcp/dhclient{}.{}.leases {} -nw {} ".format(version, file_ext, ifName, file_ext, ifName, ifName, cmd_opt)
-                print("RENEW_DHCP_LEASE - cmd {}".format(cmd))
                 output = subprocess.check_call(cmd, shell=True)
-                print('RENEW_DHCP_LEASE Output -> ', output)
+
+                output = "SUCCESS"
 
         except subprocess.CalledProcessError as err:
             print("Exception when calling get_sonic_error -> %s\n" %(err))
