dd05006bf33080b72b4a8935f9801bb8a5740c7f
Ravi Vasanthm <ravi_vasanthm@dell.com>
2020-01-08 16:03:39 -0800

    Added host module to handle dhcp lease renew command and transformer method to dhcp lease renew rpc.

---
+++ scripts/host_modules/renew_dhcp_lease.py
@@ -0,0 +1,62 @@
+""" Renew DHCP lease handler"""
+import host_service
+import subprocess
+
+MOD_NAME= 'renew_dhcp_lease'
+
+class RENEW_DHCP_LEASE(host_service.HostModule):
+    """DBus endpoint that executes RENEW_DHCP_LEASE related commands """
+
+    @staticmethod
+    def _run_command(commands, options):
+        """ Run renew dhcp lease command """
+        if len(options) < 2:
+            print("RENEW_DHCP_LEASE Invalid options, {}".format(options))
+            return 1, "Invalid options"
+
+        ifName = options[0]
+        version = "-4"
+        file_ext = "4"
+        output = ""
+        try:
+            for x in options[1:]:
+                if x == "ipv4":
+                    version = "-4"
+                    file_ext = "4"
+
+                elif x == "ipv6":
+                    version = "-6"
+                    file_ext = "6"
+                else:
+                    continue
+
+                cmd = "/sbin/dhclient {} -r {}".format(version, ifName)
+                print("RENEW_DHCP_LEASE - cmd {}".format(cmd))
+                output = subprocess.check_output(cmd)
+                print('RENEW_DHCP_LEASE release Output -> ', output)
+
+                cmd = "[ -f /var/run/dhclient{}.{}.pid ] && kill `cat /var/run/dhclient{}.{}.pid` && rm -f /var/run/dhclient{}.{}.pid".format(file_ext, ifName, file_ext, ifName, file_ext, ifName)
+                print("RENEW_DHCP_LEASE - cmd {}".format(cmd))
+                output = subprocess.check_output(cmd)
+                print('RENEW_DHCP_LEASE release Output -> ', output)
+
+                cmd = "/sbin/dhclient {} -pf /run/dhclient{}.{}.pid -lf /var/lib/dhcp/dhclient{}.{}.leases {} -nw -D LL".format(version, file_ext, ifName, file_ext, ifName, ifName)
+                print("RENEW_DHCP_LEASE - cmd {}".format(cmd))
+                output = subprocess.check_output(cmd)
+                print('RENEW_DHCP_LEASE Output -> ', output)
+
+        except subprocess.CalledProcessError as err:
+            print("Exception when calling get_sonic_error -> %s\n" %(err))
+            rc = err.returncode
+            output = err.output
+            
+        return rc,output
+
+
+    @host_service.method(host_service.bus_name(MOD_NAME), in_signature='as', out_signature='is')
+    def action(self, options):
+        return RENEW_DHCP_LEASE._run_command(options)
+        
+def register():
+    """Return class name"""
+    return RENEW_DHCP_LEASE, MOD_NAME
--- src/translib/transformer/xfmr_intf.go
+++ src/translib/transformer/xfmr_intf.go
@@ -62,6 +62,7 @@ func init () {
     XlateFuncBind("YangToDb_neigh_tbl_key_xfmr", YangToDb_neigh_tbl_key_xfmr)
     /*--show ip ARP/neighbors changes end--*/
     XlateFuncBind("rpc_clear_counters", rpc_clear_counters)
+    XlateFuncBind("rpc_renew_dhcp_lease", rpc_renew_dhcp_lease)
 }
 
 /*--show ip ARP/neighbors changes start--*/
@@ -257,6 +258,81 @@ func performIfNameKeyXfmrOp(inParams *XfmrParams, requestUriPath *string, ifName
     }
     return err
 }
+/* RPC for DHCP lease renew */
+
+var rpc_renew_dhcp_lease RpcCallpoint = func(body []byte, dbs [db.MaxDB]*db.DB) ([]byte, error) {
+    var err error
+    var result struct {
+        Output struct {
+            Status int32 `json:"status"`
+            Status_detail string`json:"status-detail"`
+        } `json:"sonic-interface:output"`
+    }
+
+    result.Output.Status = 1
+    /* Unmarshal input data */
+    var mapData map[string]interface{}
+    err= json.Unmarshal(body, &mapData)
+    if err != nil {
+        log.Info("Failed to unmarshall given input data")
+        result.Output.Status_detail = fmt.Sprintf("Error: Failed to unmarshall given input data")
+        return json.Marshal(&result)
+    }
+    input, _ := mapData["sonic-mgmt-interface:input"]
+    mapData = input.(map[string]interface{})
+    portname := mapData["portname"]
+    ifName := portname.(string)
+    log.Info("rpc_renew_dhcp_lease: intfName: ", ifName)
+
+    intfType, _, ierr := getIntfTypeByName(ifName)
+    if ierr != nil || intfType != IntfTypeMgmt {
+        log.Errorf("Extracting Interface type for Interface: %s failed or not supported!", ifName)
+        result.Output.Status_detail = fmt.Sprintf("Error: Not supported for interface " + ifName)
+        return json.Marshal(&result)
+    }
+
+    intTbl := IntfTypeTblMap[intfType]
+    tblName, _ := getIntfTableNameByDBId(intTbl, db.ConfigDB)
+    entry, dbErr := dbs[db.ConfigDB].GetEntry(&db.TableSpec{Name:tblName}, db.Key{Comp: []string{ifName}})
+    if dbErr != nil || !entry.IsPopulated() {
+        log.Errorf("Interface config not found or not populated: %s failed!", ifName)
+        result.Output.Status_detail = fmt.Sprintf("Error: DHCP client not enabled for interface " + ifName)
+        return json.Marshal(&result)
+    }
+
+    var options []string
+    options = append(options, ifName)
+    ipv4_dhcp := false
+    ipv6_dhcp := false
+    if entry.Has("ipv4_dhcp_client") && entry.Get("ipv4_dhcp_client") == "true" {
+        ipv4_dhcp = true
+        options = append(options, "ipv4")
+    }
+    if entry.Has("ipv6_dhcp_client") && entry.Get("ipv6_dhcp_client") == "true" {
+        ipv6_dhcp = true
+        options = append(options, "ipv6")
+    }
+    if (ipv4_dhcp == false && ipv6_dhcp == false) || (len(options) < 2) {
+        log.Errorf("DHCP IPv4 and IPv6 client not enabled for interface " + ifName)
+        result.Output.Status_detail = fmt.Sprintf("Error: DHCP client not enabled for interface " + ifName)
+        return json.Marshal(&result)
+    }
+
+    log.Info("rpc_renew_dhcp_lease: Command:", options)
+
+    query_result := hostQuery("renew_dhcp_lease.action", options)
+
+    if query_result.Err != nil {
+        result.Output.Status_detail = "ERROR:Internal SONiC Hostservice communication failure."
+    } else if query_result.Body[0].(int32) != 0 {
+        result.Output.Status_detail = fmt.Sprintf("ERROR:Invalid renew DHCP lease request " + ifName)
+    } else {
+        result.Output.Status = 0
+        result.Output.Status_detail = "Success: Renew DHCP lease"
+    }
+    return json.Marshal(&result)
+}
+
 
 /* RPC for clear counters */
 var rpc_clear_counters RpcCallpoint = func(body []byte, dbs [db.MaxDB]*db.DB) ([]byte, error) {
