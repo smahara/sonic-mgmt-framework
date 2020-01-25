a7ebc5394710ab10bf59916c014f38347931f4f4
arunsundark <arka6306@colorado.edu>
2020-01-25 00:49:58 +0000

    verified code

--- src/cvl/custom_validation/sonic_sflow.go
+++ src/cvl/custom_validation/sonic_sflow.go
@@ -2,29 +2,25 @@ package custom_validation
 
 import (
 	"net"
-	log "github.com/golang/glog"
+	util "cvl/internal/util"
  )
-
 
 
 //Validate ip address of sflow collector ip 
 func (t *CustomValidation) ValidateProperIp(vc *CustValidationCtxt) CVLErrorInfo {
- 	log.Info("Entered custom validation ValidateProperIp")
-	/*if (vc.YNodeName != "collector_ip") {
-	         log.Info("CVL validatuion, not called for collector ip")
-		 return CVLErrorInfo{ErrCode: CVL_FAILURE}
-	}
-*/	log.Info("Given IP sflow:",vc.YNodeVal)
 	ip := net.ParseIP(vc.YNodeVal)
 	if ip == nil {
+	         util.CVL_LEVEL_LOG(0,"%s","IP IS NIL")
                  return CVLErrorInfo{
 			            ErrCode: CVL_SYNTAX_INVALID_INPUT_DATA,
 				    TableName: "SFLOW_COLLECTOR",
 				    CVLErrDetails : "IP address is not valid",
 			    }
 	}
-	if ip.IsLoopback() || ip.IsUnspecified() || ip.IsMulticast() || ip.IsGlobalUnicast() {
-                 return CVLErrorInfo{
+
+	if ip.IsLoopback() || ip.IsUnspecified() || ip.Equal(net.IPv4bcast)|| ip.IsMulticast(){
+	        util.CVL_LEVEL_LOG(0,"%s","IP IS UNSPECIFIED OR LOOPBACK OR MULTICAST OR RESERVED")
+		return CVLErrorInfo{
 			            ErrCode: CVL_SYNTAX_INVALID_INPUT_DATA,
 				    TableName: "SFLOW_COLLECTOR",
 				    CVLErrDetails : "IP address is not valid",
