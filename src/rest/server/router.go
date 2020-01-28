4e69ccc6b7ca4241152004525a6d3891ca0cc271
Eric Seifert <seiferteric@gmail.com>
2020-01-28 10:29:39 -0800

    Set log level

--- src/rest/server/router.go
+++ src/rest/server/router.go
@@ -464,7 +464,7 @@ func authMiddleware(inner http.Handler, auth UserAuth) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		rc, r := GetContext(r)
 		rc.ClientAuth = auth
-		glog.Infof("Valid Auth Modes: %s", rc.ClientAuth)
+		glog.V(2).Infof("Valid Auth Modes: %s", rc.ClientAuth)
 		var err error
 		success := false
 		ts := time.Now()
