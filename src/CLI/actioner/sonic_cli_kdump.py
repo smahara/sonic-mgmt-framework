b76cad69a2acaa0a710491d6c0fb830700d42c14
Joyas Joseph <joyas_joseph@dell.com>
2020-01-30 09:48:33 -0800

    show kdump status

--- src/CLI/actioner/sonic_cli_kdump.py
+++ src/CLI/actioner/sonic_cli_kdump.py
@@ -68,34 +68,12 @@ def cmd_set_memory(memory):
 def cmd_set_num_dumps(num_dumps):
     run_config_cmd({"Enabled":False, "Num_Dumps":num_dumps, "Memory":""})
 
+def run(func, args):
+
+    if func == 'show':
+        if args[0] == 'kdump' and args[1] == 'status':
+            cms_show_status()
+
 if __name__ == '__main__':
 
-    if len(sys.argv) == 3:
-        if sys.argv[1] == 'show' and sys.argv[2] == 'kdump':
-            cmd_show_status()
-        elif sys.argv[1] == 'kdump' and sys.argv[2] == 'enable':
-            cmd_enable()
-        elif sys.argv[1] == 'no' and sys.argv[2] == 'kdump':
-            cmd_disable()
-    elif len(sys.argv) == 4:
-        if sys.argv[1] == 'show' and sys.argv[2] == 'kdump' and sys.argv[3] == 'status':
-            cmd_show_status()
-        elif sys.argv[1] == 'show' and sys.argv[2] == 'kdump' and sys.argv[3] == 'memory':
-            cmd_show_memory()
-        elif sys.argv[1] == 'show' and sys.argv[2] == 'kdump' and sys.argv[3] == 'num_dumps':
-            cmd_show_num_dumps()
-        elif sys.argv[1] == 'kdump' and sys.argv[2] == 'memory':
-            cmd_set_memory(sys.argv[3])
-        elif sys.argv[1] == 'kdump' and sys.argv[2] == 'num_dumps':
-            cmd_set_num_dumps(int(sys.argv[3]))
-        elif sys.argv[1] == 'no' and sys.argv[2] == 'kdump' and sys.argv[3] == 'memory':
-            cmd_set_memory("0M-2G:256M,2G-4G:320M,4G-8G:384M,8G-:448M")
-        elif sys.argv[1] == 'no' and sys.argv[2] == 'kdump' and sys.argv[3] == 'num_dumps':
-            cmd_set_num_dumps(int(3))
-    elif len(sys.argv) == 5 and sys.argv[1] == 'do':
-        if sys.argv[2] == 'show' and sys.argv[3] == 'kdump' and sys.argv[4] == 'status':
-            cmd_show_status()
-        elif sys.argv[2] == 'show' and sys.argv[3] == 'kdump' and sys.argv[4] == 'memory':
-            cmd_show_memory()
-        elif sys.argv[2] == 'show' and sys.argv[3] == 'kdump' and sys.argv[4] == 'num_dumps':
-            cmd_show_num_dumps()
+    return run(sys.argv[1], sys.argv[2:])
