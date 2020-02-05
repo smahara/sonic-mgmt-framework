df7e300e4324da43c076b15db1208511beec0ebb
Oliver Hu <Oliver_Hu@dell.com>
2020-02-05 13:32:55 -0800

    fix the issue with STRICT priority setting for scheduler

--- src/translib/transformer/xfmr_qos_scheduler.go
+++ src/translib/transformer/xfmr_qos_scheduler.go
@@ -10,7 +10,6 @@ func init () {
     XlateFuncBind("DbToYang_qos_scheduler_policy_key_xfmr", DbToYang_qos_scheduler_policy_key_xfmr)
     XlateFuncBind("YangToDb_qos_scheduler_priority_xfmr", YangToDb_qos_scheduler_priority_xfmr)
     XlateFuncBind("DbToYang_qos_scheduler_priority_xfmr", DbToYang_qos_scheduler_priority_xfmr)
-
  
 }
 
@@ -47,36 +46,31 @@ var DbToYang_qos_scheduler_policy_key_xfmr KeyXfmrDbToYang = func(inParams XfmrP
 }
 
 var YangToDb_qos_scheduler_priority_xfmr FieldXfmrYangToDb = func(inParams XfmrParams) (map[string]string, error) {
-    var db_qos_scheduler_priority string
 
     qos_scheduler_priority, _ := inParams.param.(ocbinds.E_OpenconfigQos_Qos_SchedulerPolicies_SchedulerPolicy_Schedulers_Scheduler_Config_Priority)
 
+    db_qos_scheduler_type := "WRR"
     if qos_scheduler_priority == ocbinds.OpenconfigQos_Qos_SchedulerPolicies_SchedulerPolicy_Schedulers_Scheduler_Config_Priority_STRICT {
-        db_qos_scheduler_priority = "PRIORITY"
-    } else {
-        db_qos_scheduler_priority = "WRR"
+        db_qos_scheduler_type = "PRIORITY"
     }
 
-    log.Info( "YangToDb_qos_scheduler_priority_xfmr: type: ", db_qos_scheduler_priority)
+    log.Info( "YangToDb_qos_scheduler_priority_xfmr: type: ", db_qos_scheduler_type)
     res_map :=  make(map[string]string)
-    res_map["type"] = db_qos_scheduler_priority 
+    res_map["type"] = db_qos_scheduler_type 
     return res_map, nil
 }
 
-var DbToYang_qos_scheduler_priority_xfmr KeyXfmrDbToYang = func(inParams XfmrParams) (map[string]interface{}, error) {
+var DbToYang_qos_scheduler_priority_xfmr FieldXfmrDbtoYang = func(inParams XfmrParams) (map[string]interface{}, error) {
     var err error
     rmap := make(map[string]interface{})
     data := (*inParams.dbDataMap)[inParams.curDb]
-    db_qos_scheduler_priority, ok := data["SCHEDULER"][inParams.key].Field["type"]
+    db_qos_scheduler_type, ok := data["SCHEDULER"][inParams.key].Field["type"]
     if ok {
-        log.Info("DbToYang_qos_scheduler_priority_xfmr: db_qos_scheduler_priority: ", db_qos_scheduler_priority)
-        if db_qos_scheduler_priority == "PRIORITY" {
-            rmap["priority"] = ocbinds.OpenconfigQos_Qos_SchedulerPolicies_SchedulerPolicy_Schedulers_Scheduler_Config_Priority_STRICT
-        } else {
-            rmap["priority"] = ocbinds.OpenconfigQos_Qos_SchedulerPolicies_SchedulerPolicy_Schedulers_Scheduler_Config_Priority_UNSET
+        log.Info("DbToYang_qos_scheduler_priority_xfmr: db_qos_scheduler_type: ", db_qos_scheduler_type)
+        if db_qos_scheduler_type == "PRIORITY" {
+            rmap["priority"] = "STRICT"
         }
     }
     return rmap, err
 }
-
 
