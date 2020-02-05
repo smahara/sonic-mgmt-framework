123e12484cc5a3eab3dc73b285276b6ec7fead09
Venkatesan Mahalingam <venkatesan_mahalinga@dell.com>
2020-02-05 00:06:18 -0800

    UT bug fixes

--- src/CLI/actioner/sonic_cli_bgp.py
+++ src/CLI/actioner/sonic_cli_bgp.py
@@ -1333,7 +1333,7 @@ def invoke_api(func, args=[]):
                 body = { "openconfig-network-instance:import-policy":  [ args[3] ] }
         elif attr == 'openconfig_network_instance_network_instances_network_instance_protocols_protocol_bgp_peer_groups_peer_group_afi_safis_afi_safi_apply_policy_config_export_policy':
             # openconfig_network_instance251836598
-            keypath = cc.Path('/restconf/data/openconfig-network-instance:network-instances/network-instance={name}/protocols/protocol={identifier},{name1}/bgp/peer-groups/peer-group={peer_group_name}/afi-safis/afi-safi={afi_safi_name}/apply-policy/config/openconfig-bgp-ext:export-policy',
+            keypath = cc.Path('/restconf/data/openconfig-network-instance:network-instances/network-instance={name}/protocols/protocol={identifier},{name1}/bgp/peer-groups/peer-group={peer_group_name}/afi-safis/afi-safi={afi_safi_name}/apply-policy/config/export-policy',
                 name=args[0], identifier=IDENTIFIER, name1=NAME1, peer_group_name=args[1], afi_safi_name=args[2])
             if op == OCEXTPREFIX_PATCH:
                 body = { "openconfig-network-instance:export-policy": [ args[3] ] }
