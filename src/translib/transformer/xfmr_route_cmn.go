abca6bfa0ff829f84757098e3ee6616c8688c8ad
Ashok Daparthi <Ashok_Daparthi@Dell.com>
2020-02-07 16:36:20 -0800

    Fix for redistribute ospf3 command

--- src/translib/transformer/xfmr_route_cmn.go
+++ src/translib/transformer/xfmr_route_cmn.go
@@ -82,10 +82,10 @@ var YangToDb_route_table_conn_key_xfmr KeyXfmrYangToDb = func(inParams XfmrParam
 
     if strings.Contains(srcProto, "DIRECTLY_CONNECTED") {
         source = "connected"
+    } else if strings.Contains(srcProto, "OSPF3") {
+        source = "ospf6"
     } else if strings.Contains(srcProto, "OSPF") {
         source = "ospf"
-    } else if strings.Contains(srcProto, "OSPF3") {
-        source = "ospf3"
     } else if strings.Contains(srcProto, "STATIC") {
         source = "static"
     } else {
@@ -127,7 +127,7 @@ var DbToYang_route_table_conn_key_xfmr KeyXfmrDbToYang = func(inParams XfmrParam
         src_proto = "STATIC"
     } else if source == "ospf" {
         src_proto = "OSPF"
-    } else if source == "ospf3" {
+    } else if source == "ospf6" {
         src_proto = "OSPF3"
     } else {
 		return rmap, errors.New("Unsupported src protocol " + source)
