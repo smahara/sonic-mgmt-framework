a7bf050916adab6ae2bb7763374b78c7563f9150
Justine Jose <justine_jose@dell.com>
2020-02-11 11:27:01 -0800

    Fixing the panic when IP address is configured without prefix length.

--- src/translib/transformer/xfmr_intf.go
+++ src/translib/transformer/xfmr_intf.go
@@ -1089,7 +1089,7 @@ var YangToDb_intf_ip_addr_xfmr SubTreeXfmrYangToDb = func(inParams XfmrParams) (
 	intfType, _, ierr := getIntfTypeByName(ifName)
 
     if IntfTypeVxlan == intfType {
-	    return subIntfmap, nil	
+	    return subIntfmap, nil
     }
 
     intfsObj := getIntfsRoot(inParams.ygRoot)
@@ -1103,7 +1103,7 @@ var YangToDb_intf_ip_addr_xfmr SubTreeXfmrYangToDb = func(inParams XfmrParams) (
         log.Info("YangToDb_intf_subintf_ip_xfmr : " + errStr)
         return subIntfmap, errors.New(errStr)
     }
-    
+
     if intfType == IntfTypeUnset || ierr != nil {
         errStr := "Invalid interface type IntfTypeUnset"
         log.Info("YangToDb_intf_subintf_ip_xfmr : " + errStr)
@@ -1172,7 +1172,14 @@ var YangToDb_intf_ip_addr_xfmr SubTreeXfmrYangToDb = func(inParams XfmrParams) (
                     *addr.Config.Ip = ip
                 }
                 log.Info("Ip:=", *addr.Config.Ip)
+                if addr.Config.PrefixLength == nil {
+                    log.Error("Prefix Length empty!")
+                    errStr := "Prefix Length not present"
+                    err = tlerr.InvalidArgsError{Format:errStr}
+                    return subIntfmap, err
+                }
                 log.Info("prefix:=", *addr.Config.PrefixLength)
+
                 if !validIPv4(*addr.Config.Ip) {
                     errStr := "Invalid IPv4 address " + *addr.Config.Ip
                     err = tlerr.InvalidArgsError{Format: errStr}
